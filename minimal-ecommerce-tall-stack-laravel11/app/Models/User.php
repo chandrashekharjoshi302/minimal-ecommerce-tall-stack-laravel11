d49223f2dd39d2ffdcfbc7f95a02a4fdc3bed6b3
chandrashekharjoshi302 <chandrashekharjoshi02@gmail.com>
2024-08-08 09:09:47 +0530

    add

--- minimal-ecommerce-tall-stack-laravel11/app/Models/User.php
+++ minimal-ecommerce-tall-stack-laravel11/app/Models/User.php
@@ -6,8 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Devdojo\Auth\Models\User as AuthUser;
 
-class User extends Authenticatable
+class User extends AuthUser
 {
     use HasFactory, Notifiable;
 
@@ -20,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+
     ];
 
     /**
